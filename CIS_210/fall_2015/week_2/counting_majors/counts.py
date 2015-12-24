16a1673f5313adf73658f6746eb445fa56ce99fd
Tom Rowland <tomrowland4@gmail.com>
2015-12-23 18:38:19 -0800

    new file:   counts.py 	new file:   majors.txt

---
+++ CIS_210/fall_2015/week_2/counting_majors/counts.py
@@ -0,0 +1,55 @@
+"""
+Count the number of occurrences of each major code in a file.
+Authors: #FIXME
+Credits: #FIXME
+
+Input is a file in which major codes (e.g., "CIS", "UNDL", "GEOG")
+appear one to a line. Output is a sequence of lines containing major code
+and count, one per major.
+"""
+
+import argparse
+
+def count_codes(majors_file):
+    """
+    counts and outputs the unique major codes in a file
+    args:
+        majors_file: The file which holds the major codes to be counted
+    returns:
+        nothing
+    """
+    majors = [] # create a new empty list to hold the counts
+
+    # remove newline character and append string to majors list
+    for line in majors_file:
+        majors.append(line.strip())
+
+    majors = sorted(majors) # sort the major codes alphabetically
+
+    count = {} # major codes will be keys, occurance of key will be value
+
+    for major in majors:
+        if major in count: # if the key is already present in 'count'
+            count[major] += 1 # then increment the count
+        else: # else the key is not present
+            count[major] = 1 # so set the key and give it an initial value of 1
+
+    # print the major code and count
+    for major in count:
+        print(major, count[major])
+
+def main( ):
+    """
+    Interaction if run from the command line.
+    Usage:  python3 counts.py  majors_code_file.txt
+    """
+    parser = argparse.ArgumentParser(description="Count major codes")
+    parser.add_argument('majors', type=argparse.FileType('r'),
+                        help="A text file containing major codes, one major code per line.")
+    args = parser.parse_args()  # gets arguments from command line
+    majors_file = args.majors
+    count_codes(majors_file)
+    
+    
+if __name__ == "__main__":
+    main( )
